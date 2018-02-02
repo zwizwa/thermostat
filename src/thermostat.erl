ef25d14149a61dc9e1e616a3dc781a70079f9a8a
Tom Schouten <tom@zwizwa.be>
2018-02-02 21:39:13 +0000

    first operation

--- src/thermostat.erl
+++ src/thermostat.erl
@@ -1,5 +1,7 @@
 -module(thermostat).
 -export([start/0,
+         set_furnace_state/1,
+         get_furnace_state/0,
          %% Internal
          handle/2, ping/1]).
 
@@ -10,13 +12,20 @@ start() ->
                Pid = self(), 
                {ok, Socket} = gen_udp:open(2001,[binary]),
                #{
-                  dev => 24, %% broom
+                  %% Controller Config
+                  dev => 23, %% lroom
+                  setpoint => 18.5,
+                  spread => 0.5,
+
+                  %% Infrastructure
                   socket => Socket, 
                   ping => serv:start({body, fun() -> thermostat:ping(Pid) end})
                 }
        end, 
        fun thermostat:handle/2}).
 
+%% Watchdog: sends a message every minute.  This is used by the server
+%% to shut down the furnace if no readings came in.
 ping(Pid) ->
     timer:sleep(60000),
     ok = obj:call(Pid, ping),
@@ -25,21 +34,33 @@ ping(Pid) ->
 timestamp() ->
     erlang:monotonic_time(second).
 
+%% Thermometer input.
 handle({udp, Socket, {10,1,3,From}, _, 
         <<255,255,                  %% Ad-hoc type marker
           DAC:16/little-signed>>},  %% 8.8 fixed point celcius
-       #{socket := Socket} = State) ->
+       #{socket := Socket, dev := Dev} = State) ->
     Now = timestamp(),
     Temp = DAC / 256.0,
     %%io:format("temp: ~p~n",[{From,Temp}]),
-    maps:merge(State,
+
+    %% Regulator update if device is current.
+    State0 =
+        case From of
+            Dev -> update(Temp, get_furnace_state(), State);
+            _   -> State
+        end,
+
+    %% Record
+    maps:merge(State0,
                #{ last => Now,
                   {dev, From} => {Now, Temp} });
 
+%% Debug case for bad UDP.
 handle({udp, _, _, _, _}=Msg, State) ->
     io:format("ignoring: ~p~n",[Msg]), State;
 
-handle({Pid,ping}, State = #{ dev := Dev }) ->
+%% Check if current thermometer is alive.
+handle({Pid, ping}, State = #{ dev := Dev }) ->
     obj:reply(Pid,ok),
     Now = timestamp(),
     Check =
@@ -61,17 +82,49 @@ handle({Pid,ping}, State = #{ dev := Dev }) ->
     end,
     State;
 
-handle({dev,Dev}, State) ->
-    maps:put(dev, Dev, State);
+handle({target,Dev,Setpoint}, State) ->
+    maps:merge(State,
+               #{ dev => Dev,
+                  setpoint => Setpoint });
 
 handle(Msg,State) ->
     obj:handle(Msg,State).
     
 
+%% Regulator update
+update(T, on=Old,
+       #{ spread   := Spread,
+          setpoint := Setpoint} = State) ->
+    New = case T > Setpoint + Spread/2 of
+              true  -> off;
+              false -> on
+          end,
+    transition(Old, New),
+    State;
 
-%% Watchdog: sends a message every minute.  This is used by the server
-%% to shut down the furnace if no readings came in.
+update(T, off=Old,
+       #{ spread   := Spread,
+          setpoint := Setpoint} = State) ->
+    New = case T < Setpoint - Spread/2 of
+              true -> on;
+              false -> off
+          end,
+    transition(Old, New),
+    State.
 
+%% Furnace control
+transition(Old, Old) -> ignore;
+transition(Old, New) ->
+    set_furnace_state(New),
+    io:format("~p -> ~p~n",[Old,New]).
 
+%% Always use actual pin state.  Don't duplicate states.
+get_furnace_state() ->
+    case os:cmd("/usr/local/bin/furnace.sh value") of
+        "1\n" -> off;
+        "0\n" -> on
+    end.
+set_furnace_state(on)  -> os:cmd("/usr/local/bin/furnace.sh on");
+set_furnace_state(off) -> os:cmd("/usr/local/bin/furnace.sh off").
 
-
+             
