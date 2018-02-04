01a2645666eb598eab27affafc3f89cd06f67d57
Tom Schouten <tom@zwizwa.be>
2018-02-04 18:51:30 -0500

    ice point calibration, report

--- src/thermostat.erl
+++ src/thermostat.erl
@@ -1,5 +1,6 @@
 -module(thermostat).
 -export([start/0,
+         network/0, icepoint/0,
          set_furnace_state/1,
          get_furnace_state/0,
          %% Internal
@@ -20,6 +21,7 @@ start() ->
                  dev => 23, %% lroom
                  setpoint => 18.5,
                  spread => 0.5,
+                 icepoint => icepoint(),
                  
                  %% Infrastructure
                  socket => Socket, 
@@ -46,12 +48,15 @@ timestamp() ->
 handle({udp, Socket, {10,1,3,From}, _, 
         <<255,255,                  %% Magic
           DAC:16/little-signed>>},  %% 8.8 fixed point celcius
-       #{socket := Socket, dev := Dev} = State) ->
+       #{socket := Socket, dev := Dev, icepoint := Icepoint} = State) ->
     Now = timestamp(),
-    Temp = DAC / 256.0,
+    DAC0 = maps:get(From, Icepoint, 0.0),
+    Temp = (DAC - DAC0) / 256.0,
     %%io:format("temp: ~p~n",[{From,Temp}]),
 
-    log(State,{temp,From,Temp}),
+    %% Log both uncalibrated raw DAC value, and calibrated Celcius
+    %% temperature.
+    log(State,{temp,From,DAC,Temp}),
 
     %% Regulator update if device is current.
     State0 =
@@ -105,6 +110,25 @@ handle({set_spread,Spread}=Msg, State) ->
     maps:merge(State,
                #{ spread => Spread });
 
+handle({set_calib,Calib}=Msg, State) ->
+    log(State, Msg),
+    maps:merge(State,
+               #{ calib => Calib});
+
+handle({Pid,report},State) ->
+    Network = network(),
+    Report =
+        lists:append(
+          lists:map(
+            fun({{dev,Tag},{_,T}}) ->
+                    {Name,Desc} = maps:get(Tag,Network,{unknown,<<"unknown">>}),
+                    [{Tag,Name,Desc,T,T*1.8+32}];
+               (_) -> []
+            end,
+            maps:to_list(State))),
+    obj:reply(Pid, Report),
+    State;
+
 handle(Msg,State) ->
     obj:handle(Msg,State).
     
@@ -117,24 +141,24 @@ update(T, on=Old,
               true  -> off;
               false -> on
           end,
-    transition(State, Old, New);
+    transition(State, T, Old, New);
 
 update(T, off=Old,
        #{ spread   := Spread,
           setpoint := Setpoint} = State) ->
-    New = case T < Setpoint - Spread/2 of
+    New = case T =< Setpoint - Spread/2 of
               true  -> on;
               false -> off
           end,
-    transition(State, Old, New).
+    transition(State, T, Old, New).
 
 %% Furnace control
-transition(State, Old, Old) ->
+transition(State, _, Old, Old) ->
     State;
-transition(State, Old, New) ->
+transition(State, T, Old, New) ->
     log(State, {furnace, New}),
     set_furnace_state(New),
-    io:format("~p -> ~p~n",[Old,New]),
+    io:format("~p: ~p -> ~p~n",[T, Old,New]),
     State.
 
 %% Always use actual pin state.  Don't duplicate states.
@@ -148,3 +172,60 @@ set_furnace_state(off) -> os:cmd("/usr/local/bin/furnace.sh off").
 
 log(#{log := File}, Term) ->
     file:write(File, io_lib:format("~p~n",[{timestamp(),Term}])).
+
+
+%% hostno -> {host, calib, desc}.
+network() -> 
+    #{
+       19 => {groom,      <<"Guest Room">>},  %% zora on docking station
+       18 => {garage,     <<"Garage">>},      %% pi3
+       2  => {zoo,        <<"Dining Room">>},
+       12 => {zoe,        <<"Tom Office">>},
+       23 => {lroom,      <<"Living Room">>},
+       24 => {broom,      <<"Bedroom">>},
+       97 => {beaglebone, <<"Basement">>}
+     }.
+
+%% ICE point DAC values
+icepoint() -> #{
+       2 => 592, %% zoo
+       23 => 192, %% lroom
+       24 => 160, %% broom
+       12 => 528, %% zoe
+       18 => 656, %% garage/pi3
+       19 => 944, %% groom/zora
+       97 => 336  %% beaglebone
+      }.
+
+      
+%% calib() ->
+%%     %% Honeywell thermostat as reference.
+%%     %% Host => [{Ref,Temp}].
+%%     %% Data collected as farenheit.
+%%     %% Identified by hostname.
+    
+%%     CalF = #{
+%%       18 => [{56,62.7},{56,62.8},{59,66.3},{62,68.7}],
+%%       24 => [{56,58.1},{56,58.5},{59,62.9},{62,65.0}],
+%%       97 => [{56,60.1},{56,60.6},{59,62.2},{62,65.9}],
+%%       23 => [{56,58.0},{56,58.7},{59,62.0},{62,63.8}],
+%%       12 => [{56,60.8},{56,61.2},{59,64.1},{62,66.3}],
+%%       2  => [{56,59.8},{56,60.1},{59,63.6},{62,65.6}],
+%%       19 => [{59,67.6},{62,69.2}]  %% far off, possibly bad data, was broken slot7
+%%      },
+%%     maps:map(
+%%       fun(_Name, TableF) ->
+%%               %% [{Ref,Temp}] -> Avg
+%%               SumDiffF = lists:foldl(
+%%                            fun({RefF,TempF},Acc) -> Acc + RefF - TempF end,
+%%                            0, TableF),
+%%               CorrF = SumDiffF / length(TableF),
+%%               CorrC = CorrF / 1.8,
+%%               %% Same resolution as sensor.
+%%               round(CorrC,16)
+%%       end,
+%%       CalF).
+
+%% round(C,N) -> round(C * N) / N.
+        
+
