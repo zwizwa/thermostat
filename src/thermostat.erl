998e5e988d88782b741f511d5b9ccc6acc6cf118
Tom Schouten <tom@zwizwa.be>
2018-02-05 12:03:36 -0500

    cleanups

--- src/thermostat.erl
+++ src/thermostat.erl
@@ -3,7 +3,7 @@
          network/0, icepoint/0,
          set_furnace_state/1,
          get_furnace_state/0,
-         %% Internal
+         %% Internal late binding
          handle/2, ping/1]).
 
 %% Calibration: keep the devices associated to IP addresses, and
@@ -18,10 +18,8 @@ start() ->
                {ok, Log} = file:open("/var/log/thermostat", [append, delayed_write]),
                State = #{
                  %% Controller Config
-                 dev => 23, %% lroom
-                 setpoint => 18.5,
+                 target => {lroom, 17.0},
                  spread => 0.5,
-                 icepoint => icepoint(),
                  
                  %% Infrastructure
                  socket => Socket, 
@@ -30,6 +28,8 @@ start() ->
                 },
                log(State, {start, calendar:local_time()}),
                log(State, {furnace, get_furnace_state()}),
+               log(State, {network, network()}),
+               log(State, {icepoint, icepoint()}),
                State
        end, 
        fun thermostat:handle/2}).
@@ -45,41 +45,53 @@ timestamp() ->
     erlang:monotonic_time(second).
 
 %% Thermometer input.
-handle({udp, Socket, {10,1,3,From}, _, 
+handle({udp, Socket, {10,1,3,ID}, _, 
         <<255,255,                  %% Magic
           DAC:16/little-signed>>},  %% 8.8 fixed point celcius
-       #{socket := Socket, dev := Dev, icepoint := Icepoint} = State) ->
-    Now = timestamp(),
-    DAC0 = maps:get(From, Icepoint, 0.0),
-    Temp = (DAC - DAC0) / 256.0,
-    %%io:format("temp: ~p~n",[{From,Temp}]),
+       #{socket := Socket} = State) ->
 
-    %% Log both uncalibrated raw DAC value, and calibrated Celcius
-    %% temperature.
-    log(State,{temp,From,DAC,Temp}),
-
-    %% Regulator update if device is current.
-    State0 =
-        case From of
-            Dev -> update(Temp, get_furnace_state(), State);
-            _   -> State
-        end,
-
-    %% Record
-    maps:merge(State0,
-               #{ last => Now,
-                  {dev, From} => {Now, Temp} });
+    case maps:find(ID, network()) of
+        error ->
+            log(State,{unknown, ID}),
+            io:format("unknown: ~p~n",[ID]),
+            State;
+        {ok, {Name, _Desc}} ->
+            DAC0 = maps:get(Name, icepoint(), 0.0),
+            log(State,{dac,Name,DAC}),
+            Temp = (DAC - DAC0) / 256.0,
+            self() ! {temp, Name, Temp},
+            State
+    end;
 
 %% Debug case for bad UDP.
 handle({udp, _, _, _, _}=Msg, State) ->
     io:format("ignoring: ~p~n",[Msg]), State;
 
+%% Generic temperature input.
+handle({temp, Name, Temp},
+       State = #{target := {Current, _Setpoint}}) ->
+
+    log(State,{temp,Name,Temp}),
+    
+    %% Regulator update if device is current.
+    State0 =
+        case Name of
+            Current -> update(Temp, get_furnace_state(), State);
+            _  -> State
+        end,
+    
+    %% Record
+    Now = timestamp(),
+    maps:merge(State0,
+               #{ last => Now,
+                  {dev, Name} => {Now, Temp} });
+
 %% Check if current thermometer is alive.
-handle({Pid, ping}, State = #{ dev := Dev }) ->
+handle({Pid, ping}, State = #{ target := {Current, _} }) ->
     obj:reply(Pid,ok),
     Now = timestamp(),
     Check =
-        case maps:find({dev, Dev}, State) of
+        case maps:find({dev, Current}, State) of
             {ok, {Time, _Temp}} ->
                 DT = Now - Time,
                 case DT > 60 of 
@@ -92,51 +104,50 @@ handle({Pid, ping}, State = #{ dev := Dev }) ->
     case Check of
         ok -> ok;
         {error, _}=E ->
-            io:format("ping: ~p~n",[{Dev,E}]),
-            log(State, {ping, Dev, E}),
+            io:format("ping: ~p~n",[{Current,E}]),
+            log(State, {ping, Current, E}),
             log(State, {furnace, off}),
             set_furnace_state(off)
     end,
     State;
 
-handle({set_target,Dev,Setpoint}=Msg, State) ->
+handle({set_target,Current,Setpoint}=Msg, State)
+  when is_atom(Current) and is_number(Setpoint) ->
     log(State, Msg),
     maps:merge(State,
-               #{ dev => Dev,
-                  setpoint => Setpoint });
+               #{ target => {Current, Setpoint} });
 
 handle({set_spread,Spread}=Msg, State) ->
     log(State, Msg),
     maps:merge(State,
                #{ spread => Spread });
 
-handle({set_calib,Calib}=Msg, State) ->
-    log(State, Msg),
-    maps:merge(State,
-               #{ calib => Calib});
-
-handle({Pid,report},State) ->
-    Network = network(),
-    Report =
+handle({Pid,report},State = #{target := {Current, Setpoint}}) ->
+    Descs = maps:from_list(maps:values(network())),
+    Temps =
         lists:append(
           lists:map(
-            fun({{dev,Tag},{_,T}}) ->
-                    {Name,Desc} = maps:get(Tag,Network,{unknown,<<"unknown">>}),
-                    [{Tag,Name,Desc,T,T*1.8+32}];
+            fun({{dev,Name},{_,T}}) ->
+                    Desc = maps:get(Name,Descs,<<"unknown">>),
+                    [{Name,Desc,T,c2f(T)}];
                (_) -> []
             end,
             maps:to_list(State))),
-    obj:reply(Pid, Report),
+    obj:reply(Pid, 
+              #{ target  => {Current,Setpoint,c2f(Setpoint)},
+                 furnace => get_furnace_state(),
+                 temps   => Temps }),
     State;
 
 handle(Msg,State) ->
     obj:handle(Msg,State).
     
+c2f(T) -> T*1.8+32.
 
 %% Regulator update
 update(T, on=Old,
-       #{ spread   := Spread,
-          setpoint := Setpoint} = State) ->
+       #{ spread := Spread,
+          target := {_Current, Setpoint}} = State) ->
     New = case T > Setpoint + Spread/2 of
               true  -> off;
               false -> on
@@ -144,8 +155,8 @@ update(T, on=Old,
     transition(State, T, Old, New);
 
 update(T, off=Old,
-       #{ spread   := Spread,
-          setpoint := Setpoint} = State) ->
+       #{ spread := Spread,
+          target := {_Current, Setpoint}} = State) ->
     New = case T =< Setpoint - Spread/2 of
               true  -> on;
               false -> off
@@ -188,13 +199,13 @@ network() ->
 
 %% ICE point DAC values
 icepoint() -> #{
-       2 => 592, %% zoo
-       23 => 192, %% lroom
-       24 => 160, %% broom
-       12 => 528, %% zoe
-       18 => 656, %% garage/pi3
-       19 => 944, %% groom/zora
-       97 => 336  %% beaglebone
+          zoo        => 592, 
+          lroom      => 192,
+          broom      => 160,
+          zoe        => 528,
+          garage     => 656,
+          groom      => 944,
+          beaglebone => 336
       }.
 
       
