31453a94b47e7aff9bcf61cda10b22f8c55647c9
Tom Schouten <tom@zwizwa.be>
2018-04-08 12:52:59 -0400

    cleanup

--- src/thermostat_sup.erl
+++ src/thermostat_sup.erl
@@ -3,27 +3,17 @@
 -export([start_link/0, init/1, serv_start_registered/2]).
 
 start_link() ->
-    start_cowboy(),
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
-start_cowboy() ->
-    Dispatch = cowboy_router:compile(
-                 [{'_', [{"/static/[...]", cowboy_static, {priv_dir, thermostat, "static"}}
-                        ,{"/ws",           ws, []   }      %% Websocket
-                        ,{"/[...]",        ui_thermostat, []}  %% Catch-all dispatcher
-                        ]}]),
-    NbAcceptors = 1, %% Not performance-criticial
-    _ = cowboy:start_http(http, NbAcceptors, [{port, 80}],
-                          [{env, [{dispatch, Dispatch}]}]),
-    %% Pid is never needed, so don't propagate.
-    ok.
-    
-
 init([]) ->
-    {ok,
-     {{one_for_one, 5, 5},
-      [serv_worker(thermostat, {spawner, fun thermostat:start/0})]}}.
-
+    case filelib:is_file("/usr/local/bin/furnace.sh") of
+        true ->
+            {ok,
+             {{one_for_one, 5, 5},
+              [serv_worker(thermostat, {spawner, fun thermostat:start/0})]}};
+        false ->
+            {ok, {{one_for_one, 5, 5}, []}}
+    end.
 
 %% Default record for permanent task.
 worker(Name,Mod,Fun,Args) ->
